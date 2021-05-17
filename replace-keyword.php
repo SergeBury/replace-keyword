4b319d3e6b2b37b27916415a8114efd89af679d9
Serge Bury <MY_NAME@example.com>
2021-05-17 13:02:32 +0300

    first commit

---
+++ replace-keyword.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package replace-keyword
+ * @version 1.0
+ */
+/*
+Plugin Name: Replace-keyword
+Description: Плагин в статьях добавляет для вхождения ключевых слов из keywords.txt кастомную строку.
+Author: Me
+Version: 1.0
+*/
+
+define('PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+function replace_keyword_filter_the_content($the_content)
+{
+	static $keywords = array();
+	if ( empty($keywords))
+	{
+		$keywords = explode(',',file_get_contents(PLUGIN_DIR . 'keywords.txt'));
+	}
+	
+
+	for ($i = 0, $c = count ($keywords); $i < $c; $i++)
+	{
+		$the_content = preg_replace('#'.$keywords[$i].'#iu', $keywords[$i].' в Москве', $the_content);
+
+	}
+
+	return $the_content;
+}
+
+add_filter('the_content', 'replace_keyword_filter_the_content');
+
